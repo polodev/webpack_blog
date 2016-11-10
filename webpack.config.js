8e672e49fa43c7372f5d928534c8ca7d74063aa5
polodev <polodev10@gmail.com>
2016-11-10 10:15:35 +0530

    added webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@
                  test: /\.js$/,
                  loader: 'babel-loader',
                  query: {
-                     presets: ['es2015', 'react']
+                     presets: ['es2015', 'react'],
+                     plugins : []
                  }
              }
          ]
